4d4af77f214cf43d573700336f4f517d366e515d
LeoMorgan113 <mandragora1783@gmail.com>
2021-11-21 21:44:06 +0300

    Update main.js

--- Lab3_Koriavikova/main.js
+++ Lab3_Koriavikova/main.js
@@ -226,7 +226,7 @@ function LoadTexture() {
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([0, 0, 255, 255]));
     const image = new Image();
     image.crossOrigin = 'anonymous';
-    image.src = "texture.png";
+    image.src = "./Textures/texture.png";
     image.addEventListener('load', () => {
         // Now that the image has loaded make copy it to the texture.
         gl.bindTexture(gl.TEXTURE_2D, texture);
